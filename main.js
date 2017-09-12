491cc0f0d6940503936157edc1ac16c138a02c86
Young Jun Park <june0313@gmail.com>
2017-09-12 22:43:37 +0900

    myLogger 미들웨어 적용

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ var express = require('express');
 var app = express();
 var userRouter = require('./routes/user');
 
+var myLogger = function (req, res, next) {
+    console.log(req.method);
+    console.log(req.url);
+    next();
+};
+
+app.use(myLogger);
+
+app.get('/', function (req, res) {
+    res.send("Hello World!");
+})
+
 app.use('/user', userRouter);
 
 app.listen(3000, function () {
