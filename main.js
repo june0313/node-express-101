66f2717ad8cdc62d7a3e204e177b4c7c89a20a66
Young Jun Park <june0313@gmail.com>
2017-09-12 23:03:05 +0900

    static resource 처리

--- main.js
+++ main.js
@@ -15,6 +15,9 @@ var bodyParser = require('body-parser');
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
+// 같은 경로에 대해서는 먼저 작성한 코드가 우선권을 갖는다.
+app.use('/', express.static('public'));
+
 app.get('/', function (req, res) {
     res.send("Hello World!");
 })
